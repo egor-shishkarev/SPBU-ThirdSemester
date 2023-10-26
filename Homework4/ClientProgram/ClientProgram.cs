931f2518c504e37ec5366d30dfa262a37208923d
egor-shishkarev <egorsiskarev92@gmail.com>
2023-10-26 14:48:17 +0300

    Add Get function, redesign the classes

--- Homework4/ClientProgram/ClientProgram.cs
+++ Homework4/ClientProgram/ClientProgram.cs
@@ -1,9 +1,53 @@
-﻿using SimpleFTP;
-using System.Text;
+﻿using System.Net.Sockets;
 
-var client = new Client(8888);
+string host = "127.0.0.1";
+int port = 8888;
+using TcpClient client = new();
+StreamReader? reader = null;
+StreamWriter? writer = null;
 
-var message = new StringBuilder();
+try
+{
+    client.Connect(host, port);
+    reader = new StreamReader(client.GetStream());
+    writer = new StreamWriter(client.GetStream());
+    if (writer == null || reader == null) return;
+    Task.Run(() => ReceiveMessageAsync(reader));
+    await SendMessageAsync(writer);
 
-Task.Run(client.AcceptMessage);
-await client.SendMessage();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+writer?.Close();
+reader?.Close();
+
+async Task SendMessageAsync(StreamWriter writer)
+{
+    while (true)
+    {
+        string? message = Console.ReadLine();
+        if (string.IsNullOrEmpty(message) || message == "\n") continue;
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
+    }
+}
+
+async Task ReceiveMessageAsync(StreamReader reader)
+{
+    while (true)
+    {
+        try
+        {
+            string? message = await reader.ReadLineAsync();
+            if (string.IsNullOrEmpty(message)) continue;
+            Console.WriteLine(message);
+        }
+        catch
+        {
+            break;
+        }
+    }
+}
+
--- Homework4/Server/Server.cs
+++ Homework4/Server/Server.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,98 +11,199 @@ public class Server
 
     private readonly TcpListener tcpListener;
 
-    private TcpClient tcpClient;
-
-    private StreamReader reader;
-
-    private StreamWriter writer;
-        
+    private List<Client> clients;
     public Server(int port)
     {
         this.port = port;
 
         tcpListener = new TcpListener(IPAddress.Any, this.port);
 
+        clients = new List<Client>();
+    }
+
+    protected internal void RemoveConnection(string id)
+    {
+        Client? client = clients.FirstOrDefault(c => c.Id == id);
+        if (client != null) clients.Remove(client);
+        client?.Disconnect();
     }
 
     public async Task Start()
     {
-        tcpListener.Start();
-
-        tcpClient = tcpListener.AcceptTcpClient();
-
-        Console.WriteLine($"Connection has been established with client {tcpClient.Client.LocalEndPoint}");
-
-        reader = new StreamReader(tcpClient.GetStream());
-
-        writer = new StreamWriter(tcpClient.GetStream());
-
-        while (true)
+        try
         {
-            string? data = null;
+            tcpListener.Start();
+            Console.WriteLine("Server is working. Waiting for clients...");
 
-            while (string.IsNullOrEmpty(data))
+            while (true)
             {
-                if (tcpClient.Client.Connected)
-                {
-                    data = await reader.ReadLineAsync();
-                }
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                Client newClient = new Client(tcpClient, this);
+
+                Console.WriteLine($"Connection has been established with client {tcpClient.Client.RemoteEndPoint}");
+
+                clients.Add(newClient);
+
+                Task.Run(newClient.ProcessAsync);
+
             }
-
-            Console.WriteLine($"Received data: {data}");
-            await SendReponse(data.TrimEnd());
         }
-
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
     }
 
-    private async Task SendReponse(string data)
+    protected internal async Task SendMessage(string message, string id)
     {
-        var parsedData = data.Split();
-        if (parsedData.Length != 2)
+        var client = clients.Find(c => c.Id == id);
+        if (client != null)
         {
-            await writer.WriteLineAsync("Bad request!");
-            await writer.FlushAsync();
+            await client.writer.WriteLineAsync(message);
+            await client.writer.FlushAsync();
         }
-        else if (parsedData[0] != "1")
-        {
-            await writer.WriteLineAsync("Non available fuction");
-            await writer.FlushAsync();
-        }
-        else
-        {
-            var path = parsedData[1];
-            var response = new StringBuilder();
+    }
 
-            Console.WriteLine("Ready to check directory");
-
-            if (!Directory.Exists(path))
+    protected internal async Task SendResponse(string message, string id)
+    {
+        Console.WriteLine($"Received message: {message}");
+        var client = clients.Find(c => c.Id == id);
+        if (client != null)
+        {
+            var writer = client.writer;
+            var parsedData = message.Split();
+            if (parsedData.Length != 2)
             {
-                await writer.WriteAsync("-1 \n");
+                await writer.WriteLineAsync("Bad request!");
                 await writer.FlushAsync();
+            }
+            else if (parsedData[0] == "1")
+            {
+                var path = parsedData[1];
+                if (!Directory.Exists(path))
+                {
+                    await writer.WriteAsync("-1 \n");
+                    await writer.FlushAsync();
+                }
+                else
+                {
+                    var listOfFiles = Directory.GetFiles(path);
+
+                    var listOfDirectories = Directory.GetDirectories(path);
+
+                    await writer.WriteAsync($"{listOfFiles.Length + listOfDirectories.Length} ");
+
+                    foreach (var element in listOfFiles)
+                    {
+                        await writer.WriteAsync(element.Replace("\\", "/") + " false ");
+                    }
+
+                    foreach (var element in listOfDirectories)
+                    {
+                        await writer.WriteAsync(element.Replace("\\", "/") + " true ");
+                    }
+
+                    await writer.WriteAsync("\n");
+                    await writer.FlushAsync();
+                }
+            }
+            else if (parsedData[0] == "2")
+            {
+                if (!File.Exists(parsedData[1]))
+                {
+                    await writer.WriteLineAsync("-1");
+                    await writer.FlushAsync();
+                }
+                else
+                {
+                    var bytes = File.ReadAllText(parsedData[1]);
+                    await writer.WriteAsync(new FileInfo(parsedData[1]).Length.ToString() + " " + bytes + "\n");
+                    await writer.FlushAsync();
+                }
             }
             else
             {
-                var listOfFiles = Directory.GetFiles(path);
-
-                var listOfDirectories = Directory.GetDirectories(path);
-
-                await writer.WriteAsync($"{listOfFiles.Length + listOfDirectories.Length} ");
-
-                foreach (var element in listOfFiles)
-                {
-                    await writer.WriteAsync(element.Replace("\\", "/") + " false ");
-                }
-
-                foreach (var element in listOfDirectories)
-                {
-                    await writer.WriteAsync(element.Replace("\\", "/") + " true ");
-                }
-
-                await writer.WriteAsync("\n");
-                await writer.FlushAsync();
+                await writer.WriteLineAsync("-1");
             }
         }
-        
-        Console.WriteLine("The message was sent");
+    }
+
+    private void Disconnect()
+    {
+        foreach (var client in clients) 
+        {
+            client.Disconnect();
+        }
+    }
+}
+
+public class Client
+{
+    protected internal string Id { get; } = Guid.NewGuid().ToString();
+
+    protected internal StreamReader reader;
+
+    protected internal StreamWriter writer;
+
+    protected internal TcpClient tcpClient;
+
+    private Server server;
+
+    public Client(TcpClient tcpClient, Server server)
+    {
+
+        this.tcpClient = tcpClient;
+
+        this.server = server;
+
+        var stream = tcpClient.GetStream();
+
+        reader = new StreamReader(stream);
+
+        writer = new StreamWriter(stream);
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            await server.SendMessage("You connected to server", this.Id);
+
+            while (true)
+            {
+                try
+                {
+                    string message = await reader.ReadLineAsync();
+                    if (message == null) continue;
+                    await server.SendResponse(message, Id);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"The client {tcpClient.Client.RemoteEndPoint} disconnected from the server.");
+                    break;
+                }
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            server.RemoveConnection(Id);
+        }
+    }
+
+    public void Disconnect()
+    {
+        tcpClient.Close();
+        writer.Close();
+        reader.Close();
     }
 }
