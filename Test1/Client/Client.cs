c4aed226d0b771c90163c3f2cf1c053d6bfaa9b9
egor-shishkarev <egorsiskarev92@gmail.com>
2023-11-02 16:20:00 +0300

    Add classes Server and Client

---
+++ Test1/Client/Client.cs
@@ -0,0 +1,83 @@
+﻿using System.Net.Sockets;
+using System.Reflection;
+
+namespace Chat;
+
+public class Client
+{
+    private int port;
+
+    private string host;
+
+    private TcpClient client;
+
+    private NetworkStream stream;
+
+    private StreamReader reader;
+
+    private StreamWriter writer;
+
+    public Client(int port, string host)
+    {
+        this.port = port;
+        this.host = host;
+    }
+
+    public async Task Start()
+    {
+        try
+        {
+            client = new TcpClient(host, port);
+            stream = client.GetStream();
+            Console.WriteLine("Connected to the server");
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+            Task.Run(SendMessage);
+            await ReceiveMessage();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    private async Task ReceiveMessage()
+    {
+        while (true)
+        {
+            var message = await reader.ReadLineAsync();
+            if (message == "exit")
+            {
+                //Disconnect();
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"{message}");
+        }
+    }
+
+    private async Task SendMessage()
+    {
+        while (true)
+        {
+            var message = Console.ReadLine();
+            if (message == "exit")
+            {
+                //Disconnect();
+                Environment.Exit(0);
+            }
+            await writer.WriteLineAsync(message);
+            await writer.FlushAsync();
+        }
+    }
+
+    private void Disconnect()
+    {
+        reader.Close();
+        writer.Close();
+        client.Close();
+    }
+}
---
+++ Test1/Server/Server.cs
@@ -0,0 +1,89 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Chat;
+
+public class Server
+{
+    private int port;
+
+    private TcpListener listener;
+
+    private NetworkStream stream;
+
+    private StreamReader reader;
+
+    private StreamWriter writer;
+
+    private TcpClient client;
+
+    public Server(int port)
+    {
+        this.port = port;
+        listener = new TcpListener(IPAddress.Any, port);
+
+    }
+
+    public async Task Start()
+    {
+        try
+        {
+            listener.Start();
+            Console.WriteLine("Server is working, waiting for clients...");
+            while (true)
+            {
+                client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Connected to the client");
+                stream = client.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+                Task.Run(SendMessage);
+                await ReceiveMessage();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    private async Task ReceiveMessage()
+    {
+        while (true)
+        {
+            var message = await reader.ReadLineAsync();
+            if (message == "exit")
+            {
+                //Disconnect();
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"{message}");
+        }
+    }
+
+    private async Task SendMessage()
+    {
+        while (true)
+        {
+            var message = Console.ReadLine();
+            if (message == "exit")
+            {
+                //Disconnect();
+                Environment.Exit(0);
+            }
+            await writer.WriteLineAsync(message);
+            await writer.FlushAsync();
+        }
+    }
+
+    private void Disconnect()
+    {
+        writer.Close();
+        reader.Close();
+        client.Close();
+    }
+}
