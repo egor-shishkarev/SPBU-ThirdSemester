60571d3edfc5ef39cd03ce4bae80f2c8f93ef8ec
egor-shishkarev <egorsiskarev92@gmail.com>
2023-11-30 16:45:27 +0300

    Add async implementation of check-sum

---
+++ Test2/CheckSumTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CheckSumTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
